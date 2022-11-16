cfb8063535ab0ee84410326c350ce69009e53224
dyvyd-ky <john.ky.mlv@gmail.com>
2022-11-16 20:36:23 +0300

    production

--- versone/settings/prod.py
+++ versone/settings/prod.py
@@ -6,6 +6,19 @@ except ImportError:
 DEBUG = False
 ALLOWED_HOSTS = ['www.sokonisoko.com', 'sokonisoko.com']
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 
