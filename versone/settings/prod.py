f71413e22cf036e96b7c87352a859708ce9fc816
dyvyd-ky <john.ky.mlv@gmail.com>
2022-12-10 12:15:44 +0300

    statics

--- versone/settings/prod.py
+++ versone/settings/prod.py
@@ -27,7 +27,7 @@ AWS_S3_FILE_OVERIDE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
