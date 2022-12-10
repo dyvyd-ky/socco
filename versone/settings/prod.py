448ded59fc90ed609acf256d87908d589ed19678
dyvyd-ky <john.ky.mlv@gmail.com>
2022-12-10 12:45:17 +0300

    stor

--- versone/settings/prod.py
+++ versone/settings/prod.py
@@ -42,7 +42,7 @@ MEDIA_URL = '/media/'
 #]
 #STATIC_URL = '{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
