8d6268194aaa2e65ef50e9ed6f4b3213ecdb4341
dyvyd-ky <john.ky.mlv@gmail.com>
2022-12-13 11:35:37 +0300

    yenom

--- versone/settings/prod.py
+++ versone/settings/prod.py
@@ -57,16 +57,16 @@ MEDIA_URL = '/media/'
 
 
 
-#MPESA_ENVIRONMENT = 'production'
+MPESA_ENVIRONMENT = 'production'
 
 # Credentials for the daraja app
 
-#MPESA_CONSUMER_KEY = env('MPESA_CONSUMER_KEY')
-#MPESA_CONSUMER_SECRET = env('MPESA_CONSUMER_SECRET')
+MPESA_CONSUMER_KEY = env('MPESA_CONSUMER_KEY')
+MPESA_CONSUMER_SECRET = env('MPESA_CONSUMER_SECRET')
 
 #Shortcode to use for transactions. For sandbox  use the Shortcode 1 provided on test credentials page
 
-#MPESA_SHORTCODE = '6118954'
+MPESA_SHORTCODE = env('MPESA_SHORTCODE')
 
 # Shortcode to use for Lipa na MPESA Online (MPESA Express) transactions
 # This is only used on sandbox, do not set this variable in production
@@ -79,20 +79,20 @@ MEDIA_URL = '/media/'
 # - paybill (For Paybill)
 # - till_number (For Buy Goods Till Number)
 
-#MPESA_SHORTCODE_TYPE = 'till_number'
+MPESA_SHORTCODE_TYPE = 'till_number'
 
 # Lipa na MPESA Online passkey
 # Sandbox passkey is available on test credentials page
 # Production passkey is sent via email once you go live
 
-#MPESA_PASSKEY = env('MPESA_PASSKEY')
+MPESA_PASSKEY = env('MPESA_PASSKEY')
 # Username for initiator (to be used in B2C, B2B, AccountBalance and TransactionStatusQuery Transactions)
 
-#MPESA_INITIATOR_USERNAME = env('MPESA_INITIATOR_USERNAME')
+MPESA_INITIATOR_USERNAME = env('MPESA_INITIATOR_USERNAME')
 
 # Plaintext password for initiator (to be used in B2C, B2B, AccountBalance and TransactionStatusQuery Transactions)
 
-#MPESA_INITIATOR_SECURITY_CREDENTIAL = env('MPESA_INITIATOR_SECURITY_CREDENTIAL')
+MPESA_INITIATOR_SECURITY_CREDENTIAL = env('MPESA_INITIATOR_SECURITY_CREDENTIAL')
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
