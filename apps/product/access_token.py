ad5acd3ee6f46899d118390f0c448efec4449437
dyvyd-ky <john.ky.mlv@gmail.com>
2022-12-14 16:40:32 +0300

    prod

--- apps/product/access_token.py
+++ apps/product/access_token.py
@@ -6,14 +6,16 @@ from django.conf import settings
 
 def generate_access_token():
 
-    consumer_key = settings.MPESA_CONSUMER_KEY
-    consumer_secret = settings.MPESA_CONSUMER_SECRET
+    consumer_key = settings.prod.MPESA_CONSUMER_KEY
+    consumer_secret = settings.prod.MPESA_CONSUMER_SECRET
     api_URL = 'https://api.safaricom.co.ke/oauth/v1/generate'
 
     try:
         r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
     except:
         r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret), verify=False)
+
+    print(r.text)
 
     json_response = (
         r.json()
