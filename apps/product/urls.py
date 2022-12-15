fbc6269f372edaf4056e07b2c2c2f05b575fe2c9
dyvyd-ky <john.ky.mlv@gmail.com>
2022-12-15 14:54:56 +0300

    org

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -5,7 +5,7 @@ from .api import *
 
 urlpatterns = [
     path('search/', views.search, name='search'),
-    
+    path('daraja/stk-push/', stk_push_callback, name='mpesa_stk_push_callback'),
     #api
     path('api/add_to_cart/', api_add_to_cart, name='api_add_to_cart'),
     path('api/remove_from_cart/', api_remove_from_cart, name='api_remove_from_cart'),
