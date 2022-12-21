81e1451b72c7526ee882552e9b3793e01383f89e
dyvyd-ky <john.ky.mlv@gmail.com>
2022-12-21 22:23:25 +0300

    crazy

--- apps/product/api.py
+++ apps/product/api.py
@@ -71,7 +71,7 @@ def create_checkout_session(request):
         order.paid = False
         order.save()
 
-    return response
+    return Response(response)
 
 def api_add_to_cart(request):
     data = json.loads(request.body)
