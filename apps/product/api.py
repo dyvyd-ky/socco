6fa2c449b03602e4a049be43a40cc409e0d6392b
dyvyd-ky <john.ky.mlv@gmail.com>
2022-12-22 08:51:37 +0300

    rghthfth

--- apps/product/api.py
+++ apps/product/api.py
@@ -64,7 +64,7 @@ def create_checkout_session(request):
         decrement_product_quantity(order)
         
         r = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
-        return JsonResponse(r.response_description, safe=False)
+        return JsonResponse(r, safe=False)
         
         
         #send_order_confirmation(order)
