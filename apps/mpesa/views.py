f4938bd298146e1ab610dba7864d26ee69b6803a
dyvyd-ky <john.ky.mlv@gmail.com>
2022-12-22 17:12:29 +0300

    infodata

--- apps/mpesa/views.py
+++ apps/mpesa/views.py
@@ -15,48 +15,29 @@ class LNMCallbackUrlAPIView(CreateAPIView):
     def create(self, request):
 
         #request_data = request.data
-        request_data = request.data
-        print(request_data)
-        '''merchant_request_id = request_data["Body"]["stkCallback"]["MerchantRequestID"]
-        checkout_request_id = request_data["Body"]["stkCallback"]["CheckoutRequestID"]
-        result_code = request_data["Body"]["stkCallback"]["ResultCode"]
-        result_description = request_data["Body"]["stkCallback"]["ResultDesc"]
-        amount = request_data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][0][
-            "Value"
-        ]
-        mpesa_receipt_number = request_data["Body"]["stkCallback"]["CallbackMetadata"][
-            "Item"
-        ][1]["Value"]
-        transaction_date = request_data["Body"]["stkCallback"]["CallbackMetadata"][
-            "Item"
-        ][2]["Value"]
+        payload = request.data
+        info = {}
+        callback = payload['Body']['stkCallback']
+        info['ResultCode'] = callback['ResultCode']
+        info['ResultDesc'] = callback['ResultDesc']
+        info['MerchantRequestID'] = callback['MerchantRequestID']
+        info['CheckoutRequestID'] = callback['CheckoutRequestID']
+        metadata = callback.get('CallbackMetadata')
+        if metadata:
+            metadata_items = metadata.get('Item')
+            for item in metadata_items:
+                info[item['Name']] = item.get('Value')
 
-        phone_number = request_data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][
-            3
-        ]["Value"]
-
-        from datetime import datetime
-
-        str_transaction_date = str(transaction_date)
-        print(str_transaction_date, "this should be an str_transaction_date")
-
-        transaction_datetime = datetime.strptime(str_transaction_date, "%Y%m%d%H%M%S")
-        print(transaction_datetime, "this should be an transaction_datetime")
-
-        import pytz
-        aware_transaction_datetime = pytz.utc.localize(transaction_datetime)
-        print(aware_transaction_datetime, "this should be an aware_transaction_datetime")
-
-
+        
         our_model = LNMOnline.objects.create(
-            CheckoutRequestID=checkout_request_id,
-            MerchantRequestID=merchant_request_id,
-            Amount=amount,
-            ResultCode=result_code,
-            ResultDesc=result_description,
-            MpesaReceiptNumber=mpesa_receipt_number,
-            TransactionDate=aware_transaction_datetime,
-            PhoneNumber=phone_number,
+            CheckoutRequestID=info['CheckoutRequestID'],
+            MerchantRequestID=info['MerchantRequestID'],
+            Amount=info[item['Amount']],
+            ResultCode=info['ResultCode'],
+            ResultDesc=info['ResultDesc'],
+            MpesaReceiptNumber=info[item['MpesaReceiptNumber']],
+            TransactionDate=info[item['TransactionDate']],
+            PhoneNumber=info[item['PhoneNumber']]
         )
 
         our_model.save()
@@ -64,5 +45,5 @@ class LNMCallbackUrlAPIView(CreateAPIView):
         from rest_framework.response import Response
 
         return Response({"OurResultDesc": "..and it all worked out!"})
-'''
+
 
